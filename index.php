5e5ea14504fc1832f5f1316cb7d7c7e1a043a419
kojouchearthur <kojouchearthur@yahoo.com>
2020-04-18 06:23:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,10 +20,10 @@ function getuniqueid(int $nm=4,int $lt=0){
 }
 
 #Example 1 to use the getuniqueid() function 
- var userregnum = getuniqueid() //calls the function without any parameters, hence the function will return a result (a unique id that is 4 in length, all numbers not alphabets)
+ $userregnum = getuniqueid() //calls the function without any parameters, hence the function will return a result (a unique id that is 4 in length, all numbers not alphabets)
  
 #Example 2 to use the getuniqueid() function
- var regnum = getuniqueid(4,3) //function will return an alphanumeric id that is 7 in length (4 numbers, 3 alphabets)
+ $regnum = getuniqueid(4,3) //function will return an alphanumeric id that is 7 in length (4 numbers, 3 alphabets)
  
 ##You can swap the position of the generated alphabets and numbers by modifying their position on line 18 in the function definition.
 
