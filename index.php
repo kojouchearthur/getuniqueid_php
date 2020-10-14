0189c6a19f85941ac226124bfe649dc3ae5af900
kojouchearthur <kojouchearthur@yahoo.com>
2020-10-14 06:58:25 +0100

    more efficient code with swap & case option
    Added option to specify as parameters, to:
    - output Upper or Lower case letters
    - output letters before numbers

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <?php
 
 #creating the getuniqueid function to generate Alphanumeric unique ids
-#function is defined with two parameters of type int; default parameter values are 4 and 0.
+#function is defined with four parameters; two of type int and two of type bool; default parameter values are 4, 0, true, and false.
 
-function getuniqueid(int $nm=4,int $lt=0){
-    $getnum = ''; //variable to hold digits generated
+function getuniqueid(int $nm=4,int $lt=0,bool $upr=true, bool $swap=false){
+    $getnum = ''; //variable to hold numbers generated
     while(strlen($getnum)< $nm){
-        $getnum .= mt_rand(1,10);
+        $getnum .= mt_rand(0,9);
     }
     
     $getlet = ''; //variable to hold letters generated
-    while(strlen($getlet) < $lt){
-        $lett = range('A','Z');
+    $lett = ($upr) ? range('A','Z') : range('a','z'); //check if user specified to use uppercase letters
+    while(strlen($getlet) < $lt){        
         $getlet .= $lett[mt_rand(0,25)];
     }
-    
-    $uniqueid = $getnum.$getlet; //combining digits and letters to form alphanumeric unique id
+
+    //check if user specified to place letters before numbers
+    $uniqueid = ($swap) ? $getlet.$getnum : $getnum.$getlet;
+
     return $uniqueid;
 }
 
 #Example 1 to use the getuniqueid() function 
- $userregnum = getuniqueid() //calls the function without any parameters, hence the function will return a result (a unique id that is 4 in length, all numbers not alphabets)
+ $regnum = getuniqueid() //calls the function without any parameters, hence the function will return a result (a unique id that is 4 characters in length, all numbers without letters)
  
 #Example 2 to use the getuniqueid() function
- $regnum = getuniqueid(4,3) //function will return an alphanumeric id that is 7 in length (4 numbers, 3 alphabets)
- 
-##You can swap the position of the generated alphabets and numbers by modifying their position on line 18 in the function definition.
+ $regnum = getuniqueid(4,3) //function will return an alphanumeric id that is 7 characters in length (4 numbers, 3 alphabets in upper case, e.g. 9845HTD)
+
+#Example 3 to use the getuniqueid() function
+ $regnum = getuniqueid(3,2,false,true) //function will return an alphanumeric id that is 5 characters in length (3 numbers, 2 alphabets in lowercase, with alphabets before letters, eg. rh839)    
+
 
 ?>
